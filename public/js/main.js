4d8cdc81d551c1434711ce208093e107f2ba0976
Sushrut Shringarputale <sush.shring@gmail.com>
2017-05-21 11:08:49 +0530

    Added skills section

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ function dd_scrolltotop(duration){
 	duration = duration || 500;
 	var rootel = (document.compatMode =="BackCompat")? document.body : document.documentElement;
 	if (rootel.scrollTop === 0) // in some browsers such as Chrome, use document.body instead of document.documentElement
-		rootel = document.body;
+    rootel = document.body;
 	var curscrolltop = rootel.scrollTop, scrolltimer, elapsedtime, starttime = new Date().getTime(), animatedegree = 0;
 	var totaldis = curscrolltop;
 	clearTimeout(scrolltimer);
@@ -13,25 +13,25 @@ function dd_scrolltotop(duration){
 			scrolltimer = setTimeout(function(){jumptop();}, 10);
 		}
 	}
-
+    
 	jumptop();
 }
 
 $('document').ready(function() {
-  if ($(window).width() < 768) {
-    console.log("on phone");
-    $('nav').removeClass('hidden-sm');
-    $('nav').removeClass('hidden-xs');
-    $('nav').addClass('navbar-fixed-bottom');
-  }
-  $(window).on('resize', function() {
-    if($(this).width < 786) {
-      $('nav').removeClass('hidden-sm');
-      $('nav').removeClass('hidden-xs');
-      $('nav').addClass('navbar-fixed-bottom');
+    if ($(window).width() < 768) {
+        console.log("on phone");
+        $('nav').removeClass('hidden-sm');
+        $('nav').removeClass('hidden-xs');
+        $('nav').addClass('navbar-fixed-bottom');
     }
-  });
-
+    $(window).on('resize', function() {
+        if($(this).width < 786) {
+            $('nav').removeClass('hidden-sm');
+            $('nav').removeClass('hidden-xs');
+            $('nav').addClass('navbar-fixed-bottom');
+        }
+    });
+    
     $('.chart').easyPieChart({		
 		barColor:'#26625B',//Pie chart colour
 		trackColor: '#e8e8e8',
@@ -42,24 +42,31 @@ $('document').ready(function() {
 			$(this.el).find('span').text(Math.round(percent));
 		}
 	});  
-  $(window).on('scroll reload load', function(e) {
+    $(window).on('reload load', function(e) {
+        if ($(this).scrollTop() >= $(".page-nav-space-holder").offset().top) {
+            $(".page-nav-wrapper").addClass('fixed');
+        } else {
+            $(".page-nav-wrapper").removeClass('fixed');
+        }
+    });
+    $(window).on('scroll', function(e) {
         var scrollTop = $(this).scrollTop();
         var navHeight = $(".page-nav-space-holder").offset().top;
-
+        
         if (scrollTop <= 20) {
             $('#page-nav li').each(function () {
                 $(this).removeClass('active');
             })
         } else {
-        $("#page-nav li").each(function() {
-            var curLink = $(this).find("a").first();
-            var anchorEl = $(curLink.attr("href"));
-            if (anchorEl.offset().top <= scrollTop + navHeight && anchorEl.offset().top + anchorEl.height() > scrollTop) {
+            $("#page-nav li").each(function() {
+                var curLink = $(this).find("a").first();
+                var anchorEl = $(curLink.attr("href"));
+                if (anchorEl.offset().top <= scrollTop + navHeight && anchorEl.offset().top + anchorEl.height() > scrollTop) {
                     $(this).addClass('active').siblings().removeClass('active');
-            }
-        });
+                }
+            });
         }
-
+        
         if (scrollTop >= navHeight) {
             $(".page-nav-wrapper").addClass('fixed');
         } else {
@@ -70,7 +77,7 @@ $('document').ready(function() {
         console.log($(e.target).attr('href'));
         scrollToID($(e.target).attr('href'), 1000);
     });
-
+    
 	function scrollToID(id, speed){
         var offSet = $(".page-nav-space-holder").height()+20;
         var targetOffset = $(id).offset().top - offSet;
